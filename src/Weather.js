4d5755f7035b43ceb5e64509bb399db012f5bf91
Astha Sharma <asthasharma@Asthas-MacBook-Pro.local>
2019-12-17 16:40:41 -0600

    updated with props

--- src/Weather.js
+++ src/Weather.js
@@ -1,98 +1,44 @@
 import React, { Component } from 'react'
 
+
+const Card = props =>{
+    const rows = props.param.map((row,index) => {
+        return(
+            <div key={index} className='col-sm-2 col-md-2'>
+            <div className='card'>
+            <div className='card-header text-center'>{row.day}</div>
+            <br></br>
+            <img className='icons card-img-top' src={require('./images/cloudy.png')} alt={row.climate}></img>
+            <div className='row card-body'>
+                <p className='col-xs-1 card-text'>{row.mintemp}&#176;</p>
+                <hr />
+                <p className='col-xs-1 card-text'>{row.maxtemp}&#176;</p>
+            </div>
+        </div>
+        </div>
+
+        )
+    })
+   return (
+    <div className='row'>
+        
+            {rows}
+        
+    </div>
+   )
+}
+
+
+
 class Weather extends Component{
     render(){
+        const {param} = this.props
         return(
             <div className='container'>
                 <div className='row'>
                     <h2>Weekly Weather Report</h2>
-                    </div>
-                <div className='row'>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card'>
-                            <div className='card-header text-center'>Mon</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./cloudy.png')} alt='cloudy'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card'>
-                            <div className='card-header text-center'>Tue</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./images/rain.png')} alt='rainy'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card'>
-                            <div className='card-header text-center'>Wed</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./images/sunny.png')} alt='sunny'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card'>
-                            <div className='card-header text-center'>Thur</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./cloudy.png')} alt='cloudy'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card'>
-                            <div className='card-header text-center'>Fri</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./images/rain.png')} alt='rainy'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card text-center'>
-                            <div className='card-header'>Sat</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./images/sunny.png')} alt='sunny'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-sm-2 col-md-2'>
-                        <div className='card text-center'>
-                            <div className='card-header'>Sun</div>
-                            <br />
-                            <img className='icons card-img-top' src={require('./images/sunny.png')} alt='sunny'/>
-                            <div className='row card-body'>
-                                <p className='col-xs-1 card-text'>40&#176;</p>
-                                <hr />
-                                <p className='col-xs-1 card-text'>80&#176;</p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
+                <Card param={param} />
                 
             </div> 
         )
