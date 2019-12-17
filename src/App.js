4495ac69dd0c72b29436f116cf6d72cd605a6449
Astha Sharma <asthasharma@Asthas-MacBook-Pro.local>
2019-12-17 11:36:44 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Weather from './Weather';
+
+class App extends Component{
+    render(){
+        return(
+            <div className='App'>
+                <Weather />
+            </div>
+        );
+    }
+}
+
+export default App;
