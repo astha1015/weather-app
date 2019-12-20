32ac7fd85b355d40473cbf24a2171f639780d214
Astha Sharma <asthasharma@Asthas-MacBook-Pro.local>
2019-12-20 12:29:48 -0600

    implementing stateHooks- incomplete

--- src/App.js
+++ src/App.js
@@ -1,84 +1,88 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Weather from './Weather';
 
-class App extends Component{
-    state = {
-        param: [
-            {
-                day: 'Mon',
-                image: './images/cloudy.png',
-                climate: 'cloudy',
-                maxtemp: 80,
-                mintemp: 40,
-            },
-            {
-                day: 'Tue',
-                image: './images/rain.png',
-                climate: 'rain',
-                maxtemp: 59,
-                mintemp: 52,
-
-            },
-            {
-                day: 'Wed',
-                image: './images/sunny.png',
-                climate: 'sunny',
-                maxtemp: 70,
-                mintemp: 61,
-
-            },
-            {
-                day: 'Thu',
-                image: './images/cloudy.png',
-                climate: 'cloudy',
-                maxtemp: 60,
-                mintemp: 40,
-
-            },
-            {
-                day: 'Fri',
-                image: './images/rain.png',
-                climate: 'rain',
-                maxtemp: 76,
-                mintemp: 56,
-
-            },
-            {
-                day: 'Sat',
-                image: './images/sunny.png',
-                climate: 'sunny',
-                maxtemp: 80,
-                mintemp: 72,
-
-            },
-            {
-                day: 'Sun',
-                image: './images/sunny.png',
-                climate: 'sunny',
-                maxtemp: 80,
-                mintemp: 72,
-
-            },
-
-        ],
-    }
-    removeforecast = index =>{
-        const { param } = this.state
-
-        this.setState({
+const App = props =>{
+    const [param, setParam] = useState(
+        {
+            param: [
+                {
+                    day: 'Mon',
+                    image: './images/cloudy.png',
+                    climate: 'cloudy',
+                    maxtemp: 80,
+                    mintemp: 40,
+                },
+                {
+                    day: 'Tue',
+                    image: './images/rain.png',
+                    climate: 'rain',
+                    maxtemp: 59,
+                    mintemp: 52,
+    
+                },
+                {
+                    day: 'Wed',
+                    image: './images/sunny.png',
+                    climate: 'sunny',
+                    maxtemp: 70,
+                    mintemp: 61,
+    
+                },
+                {
+                    day: 'Thu',
+                    image: './images/cloudy.png',
+                    climate: 'cloudy',
+                    maxtemp: 60,
+                    mintemp: 40,
+    
+                },
+                {
+                    day: 'Fri',
+                    image: './images/rain.png',
+                    climate: 'rain',
+                    maxtemp: 76,
+                    mintemp: 56,
+    
+                },
+                {
+                    day: 'Sat',
+                    image: './images/sunny.png',
+                    climate: 'sunny',
+                    maxtemp: 80,
+                    mintemp: 72,
+    
+                },
+                {
+                    day: 'Sun',
+                    image: './images/sunny.png',
+                    climate: 'sunny',
+                    maxtemp: 80,
+                    mintemp: 72,
+    
+                },
+    
+            ],
+        }
+    );
+    
+    const removeforecast = index => {
+        const param = {param};
+        
+        setParam({
             param: param.filter((param,i) => {
                 return i !== index
             }),
-        })
+        }) 
     }
-    render(){
-        const { param } = this.state
-        return (
-            <div className='App'>
-                <Weather param={param} removeforecast={this.removeforecast} />
-            </div>
+
+
+    return (
+        <div className='App'>
+                <Weather param={param} removeforecast={removeforecast} />
+        </div>
         );
-    }
+
 }
+
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import WeatherCard from "./WeatherCard";
 
-class Weather extends Component {
-  render() {
-    const { param, removeforecast } = this.props
-    return (
-      <div className="container">
-        <div className="row">
-          <h2>Weekly Weather Report</h2>
-        </div>
-        <div className="row">
-          {param
-            .map((row, index) => (
-              <div className="text-center col-sm-2 col-md-2">
-              <WeatherCard key={index} {...row} removeforecast={removeforecast}>
-              </WeatherCard>
-              <button onClick={() => removeforecast(index)}>Delete</button>
-              </div>
-            ))}
-        </div>
-        
+const Weather = (props) => (
+
+    <div className="container">
+      <div className="row">
+        <h2>Weekly Weather Report</h2>
       </div>
-    );
-  }
-}
+
+      <div className="row"> {Array.from(props.param).map((row, index) => (
+              <div className="text-center col-sm-2 col-md-2">
+                <WeatherCard key={index} {...row}></WeatherCard>
+                <button onClick={() => props.removeforecast(index)}>Delete</button>
+              </div>
+            )
+          )}
+      </div> 
+    </div>
+
+);
+
+
+
+
 
 export default Weather;
